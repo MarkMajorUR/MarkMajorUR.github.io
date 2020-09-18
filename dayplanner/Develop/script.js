575b490d3a575675c899339b0b6b015ba62bc949
Mark Major <mmajor@valcv.org>
2020-09-18 19:16:07 -0400

    time function

--- dayplanner/Develop/script.js
+++ dayplanner/Develop/script.js
@@ -1,36 +1,22 @@
+function updateTime() {
+  var current_time = moment().calendar();
+  $("#currentDay").text(current_time);
+}
+
 $(document).ready(function() {
 
   $(".save-btn").click(function(event) {
     event.preventDefault();
-    // Review this
+
     var data_attribute = $(this).children().data("time");
     var form = $("#"+ data_attribute +"Message");
     var value = form.val();
     localStorage.setItem(data_attribute, value);
-    console.log(localStorage.getItem(data_attribute));
-
-    // moment.js for time sync
-    // store the values from text boxes in local storage
-    // color code schedule with past, present, and future
-
   });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  updateTime();
+  setInterval(function () {
+    updateTime();
+  }, 10000);
 
 });
